19fab878e044f286b14e377b24eda133beab2cf7
Alfredo Baez <carlos.baez@pucp.edu.pe>
2019-10-15 19:47:44 -0500

    Add the arg 'sample duration'

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ if __name__=="__main__":
         if os.path.exists(video_path):
             print(video_path)
             subprocess.call('mkdir tmp', shell=True)
-            subprocess.call('ffmpeg -i {} tmp/image_%05d.jpg'.format(video_path),
+            subprocess.call('ffmpeg -i {} -loglevel quiet tmp/image_%05d.jpg'.format(video_path),
                             shell=True)
 
             result = classify_video('tmp', input_file, class_names, model, opt)
